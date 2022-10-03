88b05e3ff22cc11c78fbfcbee014c4ed828e84af
ishaan-sangwan <69313039+ishaan-sangwan@users.noreply.github.com>
2022-10-03 23:49:17 +0530

    rectification

--- callibration.py
+++ callibration.py
@@ -1,3 +1,4 @@
+from unittest.mock import CallableMixin
 import numpy as np
 import cv2 as cv
 import glob
@@ -12,6 +13,8 @@ criteria = (cv.TERM_CRITERIA_EPS + cv.TERM_CRITERIA_MAX_ITER, 30, 0.001)
 
 objp = np.zeros((chessboardSize[0]*chessboardSize[1],3), np.float32)
 objp[:,:2] = np.mgrid[0:chessboardSize[0],0:chessboardSize[1]].T.reshape(-1,2)
+flags = cv.CALIB_CB_NORMALIZE_IMAGE | cv.CALIB_CB_EXHAUSTIVE | cv.CALIB_CB_ACCURACY
+
 
 objp = objp*20
 
@@ -31,10 +34,10 @@ for imgLeft,imgRight in zip(imagesLeft,imagesRight):
     grayL = cv.cvtColor(imgL, cv.COLOR_BGR2GRAY)
     grayR = cv.cvtColor(imgR, cv.COLOR_BGR2GRAY)
     
-    retL, cornersL = cv.findChessboardCorners(imgL, (8,6), None)
-    retR, cornersR = cv.findChessboardCorners(imgR, (8,6), None)
+    retL, cornersL = cv.findChessboardCorners(imgL, (8,6), None, flags=flags)
+    retR, cornersR = cv.findChessboardCorners(imgR, (8,6), None, flags=flags)
     
-    # print("hello world")
+    print(retL)
     if retL and retR==True:
         objpoints.append(objp)
         
@@ -51,19 +54,41 @@ for imgLeft,imgRight in zip(imagesLeft,imagesRight):
     
         # num+=1
         
-    cv.waitKey(10000)
+        cv.waitKey(10000)
 cv.destroyAllWindows()
-# ret, mtx, dist, rvecs, tvecs = cv.calibrateCamera(objpoints, imgpoints, gray.shape[::-1], None, None)
 
-# #Plot undistorted 
-# h,  w = img.shape[:2]
-# newcameramtx, roi = cv.getOptimalNewCameraMatrix(mtx, dist, (w,h), 1, (w,h))
+#------------------------------------
 
-# dst = cv.undistort(img, mtx, dist, None, newcameramtx)
-# # crop the image
-# x, y, w, h = roi
-# dst = dst[y:y+h, x:x+w]
-# plt.figure()
-# plt.imshow(dst)
-# plt.savefig("undistorted.png", dpi = 300)
-# plt.close()
+retL , cameraMatrixL, distL, rvsecsL,tvecsL =cv.calibrateCamera(objpoints, imgpointsL,frameSize, None,None)
+heightL, widthL, channelsL = imgL.shape
+newCameraMatrixL, roi_L = cv.getOptimalNewCameraMatrix(cameraMatrixL, distL,(widthL, heightL),1,(widthL, heightL))
+
+retR, cameraMatrixR, distR, rvsecsR, tvecsR = cv.calibrateCamera(objpoints, imgpointsR, frameSize, None, None)
+heightR, widthR, channelsR = imgR.shape
+newCameraMatrixR, roi_R = cv.getOptimalNewCameraMatrix(cameraMatrixR, distR, (widthR, heightR), 1,None, None)
+
+#------------------------------------
+
+flags = 0
+flags|=cv.CALIB_FIX_INTRINSIC
+
+
+criteria_stereo = (cv.TERM_CRITERIA_EPS +cv.TERM_CRITERIA_MAX_ITER,30,0.001)
+# retStereo,newCameraMatrixL, distL, newCameraMatrixR, distR, rot,trans,essentialMatrix, fundamentalMatrix=
+#----------------------------------
+
+rectifyScale = 1
+rectL, rectR, projMatrixL, projMatrixR, Q, roi_L, roi_R = cv.stereoRectify(newCameraMatrixL, distL,newCameraMatrixR, distR, grayL, grayR)
+stereomapL = cv.initUndistortRectifyMap(newCameraMatrixL, distL, rectL, projMatrixL, grayL.shape[::-1], cv.CV_16SC2)
+stereomapR = cv.initUndistortRectifyMap(newCameraMatrixR, distL, rectL, projMatrixR, grayR.shape[::-1], cv.CV_16SC2)
+
+print("saving parameters!")
+
+cv_file = cv.FileStorage("stereomap.xml", cv.FILE_STORAGE_WRITE)
+
+cv_file.write("stereoMapL_x", stereomapL[0])
+cv_file.write("stereoMapL_y", stereomapL[1])
+cv_file.write("stereoMapR_x", stereomapR[0])
+cv_file.write("stereoMapR_y", stereomapR[1])
+
+cv_file.release()
